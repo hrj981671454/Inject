66ccdb2859fdc60c55e3675479593f2df6f7384d
herenjie <15296711325@163.com>
2018-09-12 18:06:55 +0800

    注解框架实现

--- app/src/main/java/com/hrj/inject/MainActivity.java
+++ app/src/main/java/com/hrj/inject/MainActivity.java
@@ -1,13 +1,38 @@
 package com.hrj.inject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.eaju.inject.ContentView;
+import com.eaju.inject.OnClick;
+import com.eaju.inject.OnLongClick;
+import com.eaju.inject.ViewInject;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+    @ViewInject(R.id.tvText)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        textView.setText("哈哈哈哈哈哈哈");
+        textView.setTextSize(25);
     }
+
+
+    @OnClick({R.id.tvText})
+    public void onClick(View view) {
+        Toast.makeText(this, "我被点了", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnLongClick(R.id.tvText)
+    public boolean onLongClick(View view) {
+        Toast.makeText(this, "我被长按了", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
 }
